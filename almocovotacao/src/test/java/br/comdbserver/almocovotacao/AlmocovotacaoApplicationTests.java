48297fa15b110a917f2f9df18bb8f7b4772953f1
Austinmff <austinmff@outlook.com>
2026-01-09 18:54:38 -0300

    Initial commit

---
+++ almocovotacao/src/test/java/br/comdbserver/almocovotacao/AlmocovotacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.comdbserver.almocovotacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlmocovotacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
